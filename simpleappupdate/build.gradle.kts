c395193ae834d9768436402ef84210f9fe4dfba9
julio <jberzal86@gmail.com>
2024-08-16 17:41:08 +0200

    increased module version

--- simpleappupdate/build.gradle.kts
+++ simpleappupdate/build.gradle.kts
@@ -60,7 +60,7 @@ afterEvaluate {
 
                 groupId = "com.triskelapps"
                 artifactId = "simpleappupdate"
-                version = "0.0.8"
+                version = "0.1.0"
 
             }
         }
